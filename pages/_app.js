450b5a8f75f8d82bea1bd3034d9d35aa0e674af6
Paul <pcwilliams30@hotmail.com>
2025-08-02 21:34:11 -0500

    5.6

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // /pages/_app.js
-import '../styles/globals.css'
+import '/styles/globals.css'; // Critical to load Tailwind
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
