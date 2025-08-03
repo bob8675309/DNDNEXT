f4031b4932704e538d38c3cf2e889b47a4f7d427
Paul <pcwilliams30@hotmail.com>
2025-08-03 18:54:23 -0500

    006

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,19 @@
-import '../styles/globals.scss';
+import '@/styles/globals.scss';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Bootstrap 5 via CDN */}
-       <link
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-			rel="stylesheet"
-			integrity="sha384-..."
-			crossOrigin="anonymous"
-	/>
+        {/* Bootstrap 5 via CDN (no integrity or crossorigin) */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
         <script
-		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-			integrity="sha384-..."
-			crossOrigin="anonymous"
-			defer
-	></script>
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+          defer
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
