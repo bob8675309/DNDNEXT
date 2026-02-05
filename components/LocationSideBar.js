5c8a8869a35b29d170326493b2d63789978fc0b9
Paul <dndnext.vercel.app>
2026-02-04 18:34:58 -0600

    1M1

--- components/LocationSideBar.js
+++ components/LocationSideBar.js
@@ -1,9 +1,9 @@
-// components/LocationSideBar.js
+//   components/LocationSideBar.js
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { supabase } from "../utils/supabaseClient";
 
-//      Helpers
+//    Helpers
 const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
