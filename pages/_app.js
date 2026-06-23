00f6be690745a0625abae31fc04a6755d3a8157d
bob8675309 <69051502+bob8675309@users.noreply.github.com>
2026-06-23 13:36:03 -0500

    Load equipment diagram styles

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/globals.scss";
 import "../styles/npc-forge.scss";
 import "../styles/card-compact.css";
 import "../styles/npc-profile-panel.css";
+import "../styles/equipment-diagram.css";
 import AppNavbar from "../components/AppNavbar";
 import Head from "next/head";
 
