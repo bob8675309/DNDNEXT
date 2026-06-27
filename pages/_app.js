3740e7a898e7c9c6bef345050abf6de89e532f26
bob8675309 <69051502+bob8675309@users.noreply.github.com>
2026-06-27 14:14:43 -0500

    Load admin build badge styles

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import "../styles/equipment-diagram.css";
 import "../styles/equipment-diagram-three-column.css";
 import "../styles/equipment-clean-overrides.css";
 import "../styles/equipment-send-controls.css";
+import "../styles/admin-build-badge.css";
 import AppNavbar from "../components/AppNavbar";
 import AdminBuildBadge from "../components/AdminBuildBadge";
 import Head from "next/head";
