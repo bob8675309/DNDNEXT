2aca443117ce157a2b23ab3c7e8b9e9aba5569a0
bob8675309 <69051502+bob8675309@users.noreply.github.com>
2026-06-28 00:11:32 -0500

    Resolve craft capability in interaction wrapper

--- components/character/CharacterInteractionPanel.js
+++ components/character/CharacterInteractionPanel.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NpcPanel from "../NpcPanel";
+import { resolveCraftProfession } from "../../utils/craftProfession";
 
 export const CHARACTER_INTERACTION_VIEWS = ["profile", "sheet", "inventory", "shop", "craft"];
 
@@ -8,11 +9,20 @@ export function normalizeCharacterInteractionView(value) {
   return CHARACTER_INTERACTION_VIEWS.includes(view) ? view : "profile";
 }
 
+function sheetForCraftResolution(character) {
+  return character?.character_sheet || character?.sheet || character?.sheet_json || {};
+}
+
 export default function CharacterInteractionPanel({ character = null, npc = null, initialView = "profile", ...props }) {
   const panelCharacter = character || npc;
+  const craftProfession = resolveCraftProfession(panelCharacter || {}, sheetForCraftResolution(panelCharacter));
+  const hasCraftCapability = !!craftProfession && craftProfession !== "Scribe";
+
   return React.createElement(NpcPanel, {
     ...props,
     npc: panelCharacter,
     initialView: normalizeCharacterInteractionView(initialView),
+    craftProfession,
+    hasCraftCapability,
   });
 }
