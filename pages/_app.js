6eb965771eed0d9bdf9ae2a6c2d048fdca9f83ad
bob8675309 <69051502+bob8675309@users.noreply.github.com>
2026-07-25 14:36:09 -0500

    Load background info layout after forge styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import "../styles/globals.scss";
 import "../styles/npc-forge.scss";
 import "../styles/npc-forge-v2.css";
+import "../styles/npc-forge-background-info.css";
 import "../styles/card-compact.css";
 import "../styles/npc-profile-panel.css";
 import "../styles/npc-page-controls.css";
