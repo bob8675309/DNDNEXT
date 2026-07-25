6d9fca62c46fe9f2cb5e8df8df5714ca5aa76a70
bob8675309 <69051502+bob8675309@users.noreply.github.com>
2026-07-25 10:42:25 -0500

    Separate background feature neutralization from lore overrides

--- utils/backgroundMechanics.js
+++ utils/backgroundMechanics.js
@@ -1,1 +1,21 @@
+import { backgroundFeatureDetails as refinedBackgroundFeatureDetails } from "./backgroundMechanicsRefined.js";
+import {
+  neutralizeBackgroundFeature,
+  playerFacingBackgroundFeatureName,
+} from "./backgroundNeutralization.js";
+
 export * from "./backgroundMechanicsRefined.js";
+
+export function backgroundFeatureDetails(background = {}) {
+  const sourceName = background.sourceName || background.source_name || background.name || background.key || "";
+  const rawFeatures = refinedBackgroundFeatureDetails({
+    ...background,
+    name: `${sourceName || "background"}__feature`,
+  });
+
+  return rawFeatures.map((feature) => ({
+    ...feature,
+    name: playerFacingBackgroundFeatureName(sourceName, feature.name),
+    description: neutralizeBackgroundFeature(sourceName, feature.name, feature.description),
+  }));
+}
