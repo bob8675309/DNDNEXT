46d0581fd20ea987853bd99196818a166c041d2a
bob8675309 <69051502+bob8675309@users.noreply.github.com>
2026-09-01 08:47:45 -0500

    Fix generated artwork ESM imports

--- utils/forgeGeneratedArt/aarakocraHero.js
+++ utils/forgeGeneratedArt/aarakocraHero.js
@@ -1,4 +1,4 @@
-import part1 from "./aarakocraHeroPart1";
-import part2 from "./aarakocraHeroPart2";
+import part1 from "./aarakocraHeroPart1.js";
+import part2 from "./aarakocraHeroPart2.js";
 
 export default `${part1}${part2}`;
