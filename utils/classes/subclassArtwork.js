816e986fc7df378bc7d559070589998710ec123d
bob8675309 <69051502+bob8675309@users.noreply.github.com>
2026-09-09 16:09:54 -0500

    Wire approved subclass artwork batch

--- utils/classes/subclassArtwork.js
+++ utils/classes/subclassArtwork.js
@@ -24,6 +24,58 @@ const WIZARD_SUBCLASS_ART_FAMILY = Object.freeze({
   war: "war",
 });
 
+const APPROVED_SUBCLASS_ART_FAMILIES = Object.freeze({
+  fighter: Object.freeze({
+    "arcane-archer": "arcane-archer",
+    banneret: "banneret",
+    "purple-dragon-knight-banneret": "banneret",
+    "battle-master": "battle-master",
+    cavalier: "cavalier",
+    champion: "champion",
+    "echo-knight": "echo-knight",
+  }),
+  paladin: Object.freeze({
+    ancients: "ancients",
+    conquest: "conquest",
+    crown: "crown",
+    devotion: "devotion",
+    glory: "glory",
+    "noble-genies": "noble-genies",
+  }),
+  ranger: Object.freeze({
+    "beast-master": "beast-master",
+    drakewarden: "drakewarden",
+    "fey-wanderer": "fey-wanderer",
+    "gloom-stalker": "gloom-stalker",
+    "horizon-walker": "horizon-walker",
+    hunter: "hunter",
+  }),
+  sorcerer: Object.freeze({
+    aberrant: "aberrant",
+    "aberrant-mind": "aberrant",
+    clockwork: "clockwork",
+    "clockwork-soul": "clockwork",
+    "divine-soul": "divine-soul",
+    draconic: "draconic",
+    shadow: "shadow",
+    wild: "wild-magic",
+    "wild-magic": "wild-magic",
+  }),
+  warlock: Object.freeze({
+    archfey: "archfey",
+    celestial: "celestial",
+    fiend: "fiend",
+    "great-old-one": "great-old-one",
+    hexblade: "hexblade",
+    undead: "undead",
+  }),
+});
+
+function approvedSubclassArtworkFor(normalizedClass = "", normalizedSubclass = "") {
+  const family = APPROVED_SUBCLASS_ART_FAMILIES[normalizedClass]?.[normalizedSubclass];
+  return family ? `/media/subclasses/${normalizedClass}/${normalizedClass}-${family}.webp` : "";
+}
+
 export function subclassArtworkFor(classKey = "", option = {}) {
   const normalizedClass = key(classKey);
   const normalizedSubclass = key(option?.name || option?.key);
@@ -31,7 +83,8 @@ export function subclassArtworkFor(classKey = "", option = {}) {
     const family = WIZARD_SUBCLASS_ART_FAMILY[normalizedSubclass];
     if (family) return `/media/subclasses/wizard/wizard-${family}.webp`;
   }
-  return classMenuArtworkFor(normalizedClass);
+  return approvedSubclassArtworkFor(normalizedClass, normalizedSubclass)
+    || classMenuArtworkFor(normalizedClass);
 }
 
 export function handleSubclassArtworkError(event, classKey = "") {
